1482c5e9cc223bea701d41e9b35a4680b34050cc
Ankita Kansra <kansrankita00@gmail.com>
2024-06-16 14:50:52 +0530

    Episode 2

---
+++ Episode 2/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", {id: "parent"}, [
+  React.createElement("div",  {id: "child"}, [
+      React.createElement("h1", {}, "I'm H1 tag!"),
+      React.createElement("h2", {}, "I'm H2 tag!")
+    ]
+  ), 
+  React.createElement("div",  {id: "child2"}, [
+      React.createElement("h1", {}, "I'm H1 tag!"),
+      React.createElement("h2", {}, "I'm H2 tag!")
+    ]
+  )] 
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
